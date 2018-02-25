bd9fd9b505ef06ccd1bdee75320acf6bce31da59
Alexandre Perrin <alex@kaworu.ch>
2018-02-25 21:01:06 +0100

    Typo.

--- travis-test-all.sh
+++ travis-test-all.sh
@@ -5,7 +5,7 @@ set -e
 DIR=$(dirname "$0")
 TRAVIS_OS_NAME=$1
 
-if [ "$TRAVIS_OS_NAME" == "linux"]; then
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     export SWIFTENV_ROOT="$HOME/.swiftenv"
     export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"
 fi
