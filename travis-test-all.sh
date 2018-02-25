f19245add7715bcaccba4a5038e7f9e2acc1449c
Alexandre Perrin <alex@kaworu.ch>
2018-02-25 20:57:52 +0100

    Alright, Travis-CI should work on both linux and osx now.

--- travis-test-all.sh
+++ travis-test-all.sh
@@ -3,9 +3,12 @@
 set -e
 
 DIR=$(dirname "$0")
+TRAVIS_OS_NAME=$1
 
-export SWIFTENV_ROOT="$HOME/.swiftenv"
-export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"
+if [ "$TRAVIS_OS_NAME" == "linux"]; then
+    export SWIFTENV_ROOT="$HOME/.swiftenv"
+    export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"
+fi
 
 # slox
 (cd "${DIR}/slox" && swift test --verbose)
