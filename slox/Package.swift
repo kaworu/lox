4bad36326c1e18a70a0fe2ea4c832aafb9a1b203
Alexandre Perrin <alex@kaworu.ch>
2019-11-23 08:37:17 +0100

    Swift 5.x related build fix

--- slox/Package.swift
+++ slox/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/sharplet/Regex.git", from: "1.1.0"),
+        .package(url: "https://github.com/sharplet/Regex.git", from: "2.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can
